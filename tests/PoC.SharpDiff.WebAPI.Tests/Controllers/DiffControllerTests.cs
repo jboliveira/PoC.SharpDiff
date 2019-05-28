3dc8280a3b7d45bc7a351421e2bf77f723d16038
Jader Oliveira <jader.oliveira@genesisautomation.healthcare>
2019-05-28 22:41:42 +0100

    Update DiffControllerTests.cs

--- tests/PoC.SharpDiff.WebAPI.Tests/Controllers/DiffControllerTests.cs
+++ tests/PoC.SharpDiff.WebAPI.Tests/Controllers/DiffControllerTests.cs
@@ -50,6 +50,22 @@ namespace PoC.SharpDiff.WebAPI.Tests.Controllers
 
 			// Assert
 			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Theory]
+		[InlineData(1, "BilacSPBrazil", "'Content' should not be null.")]
+		public static async Task CreateContentLeftAsync_ReturnsBadRequestResultWithErrorMessage_WhenResourceIsInvalid(int id, string text, string expectedErrorMessage)
+		{
+			// Arrange
+			var data = text.ConvertToBase64FromUTF8String();
+			CreateContentResource resource = new CreateContentResourceBuilder().WithData(data).Build();
+			ContentResponse response = new ContentResponse(expectedErrorMessage);
+			var sutDiffController = SetupDiffControllerForUpsertContentAsync(response);
+
+			// Act
+			var result = await sutDiffController.CreateContentLeftAsync(id, resource);
+
+			// Assert
 			Assert.Equal(expectedErrorMessage, (result as BadRequestObjectResult).Value);
 		}
 
@@ -87,7 +103,54 @@ namespace PoC.SharpDiff.WebAPI.Tests.Controllers
 
 			// Assert
 			Assert.IsType<BadRequestObjectResult>(result);
+		}
+
+		[Theory]
+		[InlineData(1, "BilacSPBrazil", "'Content' should not be null.")]
+		public static async Task CreateContentRightAsync_ReturnsBadRequestResultWithErrorMessage_WhenResourceIsInvalid(int id, string text, string expectedErrorMessage)
+		{
+			// Arrange
+			var data = text.ConvertToBase64FromUTF8String();
+			CreateContentResource resource = new CreateContentResourceBuilder().WithData(data).Build();
+			ContentResponse response = new ContentResponse(expectedErrorMessage);
+			var sutDiffController = SetupDiffControllerForUpsertContentAsync(response);
+
+			// Act
+			var result = await sutDiffController.CreateContentRightAsync(id, resource);
+
+			// Assert
 			Assert.Equal(expectedErrorMessage, (result as BadRequestObjectResult).Value);
+		}
+
+		[Fact]
+		public static async Task CompareAsync_ReturnsNotFoundResult_WhenContentLeftNotExists()
+		{
+			// Arrange
+			const int id = 1;
+			ContentResponse response = new ContentResponse($"Content left {id} not found.");
+			var sutDiffController = SetupDiffControllerForCompareAsync(id, ContentDirection.Left, response);
+
+			// Act
+			var result = await sutDiffController.CompareAsync(id);
+
+			// Assert
+			Assert.IsType<NotFoundObjectResult>(result);
+		}
+
+		[Fact]
+		public static async Task CompareAsync_ReturnsNotFoundResultErrorMessage_WhenContentLeftNotExists()
+		{
+			// Arrange
+			const int id = 1;
+			const string expectedErrorMessage = "Content left 1 not found.";
+			ContentResponse response = new ContentResponse(expectedErrorMessage);
+			var sutDiffController = SetupDiffControllerForCompareAsync(id, ContentDirection.Left, response);
+
+			// Act
+			var result = await sutDiffController.CompareAsync(id);
+
+			// Assert
+			Assert.Equal(expectedErrorMessage, (result as NotFoundObjectResult).Value);
 		}
 
 		/// <summary>
@@ -98,6 +161,18 @@ namespace PoC.SharpDiff.WebAPI.Tests.Controllers
 			var stubContentService = new Mock<IContentService>();
 			ContentResponse stubReturn = response;
 			stubContentService.Setup(s => s.UpsertContentAsync(It.IsAny<Content>())).ReturnsAsync(stubReturn);
+
+			return new DiffController(stubContentService.Object);
+		};
+
+		/// <summary>
+		/// The setup diff controller for compare async.
+		/// </summary>
+		private static readonly Func<int, ContentDirection, ContentResponse, DiffController> SetupDiffControllerForCompareAsync = (id, direction, response) =>
+		{
+			var stubContentService = new Mock<IContentService>();
+			ContentResponse stubReturn = response;
+			stubContentService.Setup(s => s.GetContentAsync(id, direction)).ReturnsAsync(stubReturn);
 
 			return new DiffController(stubContentService.Object);
 		};
