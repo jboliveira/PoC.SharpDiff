917d10ce7c5a1ef719c4b350f0ad832bf7e3e30b
Jader Oliveira <jader.bueno@yahoo.ie>
2019-10-25 11:59:33 +0200

    Fix TravisCI build (#18)
    * Refactoring
    
    - Updated `common.props`
    - Updated `*.csproj` packages and tags
    - Updated `startup.cs` and `program.cs`
    
    * Refactoring
    
    - Migrated test projs to .net core 3
    
    * Updated infra
    
    * Update Startup.cs
    
    * Updated infra
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update common.props
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update travis-build.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update travis-build.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update common.props
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update common.props
    
    * Moved all projects into .netcoreapp3
    
    * Update .travis.yml
    
    * Update common.props
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update README.md
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update travis-build.sh

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # PoC.SharpDiff
 
 [![Build Status](https://travis-ci.com/jboliveira/PoC.SharpDiff.svg?token=hoKsky9xhb4rvz3jQYmq&branch=master)](https://travis-ci.com/jboliveira/PoC.SharpDiff)
+[![Actions Status](https://github.com/jboliveira/PoC.SharpDiff/workflows/Build/badge.svg)](https://github.com/jboliveira/PoC.SharpDiff/actions)
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/aea87107017c4c3b8d52b760b32970be)](https://www.codacy.com/app/jader.oliveira/PoC.SharpDiff?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=jboliveira/PoC.SharpDiff&amp;utm_campaign=Badge_Grade)
 [![Codacy Badge](https://api.codacy.com/project/badge/Coverage/aea87107017c4c3b8d52b760b32970be)](https://www.codacy.com/app/jader.oliveira/PoC.SharpDiff?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=jboliveira/PoC.SharpDiff&amp;utm_campaign=Badge_Coverage)
--- tools/travis-build.sh
+++ tools/travis-build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Project Variables
+version = 1.0
+netConfiguration="Release"
+netCoreTargetVersion="netcoreapp3.0"
+
 # Tooling Path Variables
 runnerSonarScanner="${TRAVIS_BUILD_DIR}/tools/sonar/SonarScanner.MSBuild.dll"
 runnerCodacy="${TRAVIS_BUILD_DIR}/tools/codacy-coverage-reporter.jar"
@@ -11,18 +16,18 @@ pathIntegrationTests="${TRAVIS_BUILD_DIR}/tests/PoC.SharpDiff.Tests"
 
 # Start SonarScanner
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-	dotnet $runnerSonarScanner begin /n:"${SONAR_PROJ_NAME}" /k:"${SONAR_PROJ_KEY}" /v:"1.0.${TRAVIS_BUILD_NUMBER}" /d:sonar.login="${SONAR_LOGIN}" /o:"${SONAR_ORG}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.cs.vstest.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.trx" /d:sonar.cs.xunit.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.xml" /d:sonar.cs.opencover.reportsPaths="$pathResourcesTests/BuildReports/Coverage/coverage.opencover.xml,$pathWebAPITests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.cs.opencover.it.reportsPaths="$pathIntegrationTests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.links.ci="${LINKS_CI}" /d:sonar.links.issue="${LINKS_ISSUE}" /d:sonar.links.scm="${LINKS_SCM}"
+	dotnet $runnerSonarScanner begin /n:"${SONAR_PROJ_NAME}" /k:"${SONAR_PROJ_KEY}" /v:"$version.${TRAVIS_BUILD_NUMBER}" /d:sonar.login="${SONAR_LOGIN}" /o:"${SONAR_ORG}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.cs.vstest.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.trx" /d:sonar.cs.xunit.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.xml" /d:sonar.cs.opencover.reportsPaths="$pathResourcesTests/BuildReports/Coverage/coverage.opencover.xml,$pathWebAPITests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.cs.opencover.it.reportsPaths="$pathIntegrationTests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.links.ci="${LINKS_CI}" /d:sonar.links.issue="${LINKS_ISSUE}" /d:sonar.links.scm="${LINKS_SCM}"
 else
-    dotnet $runnerSonarScanner begin /n:"${SONAR_PROJ_NAME}" /k:"${SONAR_PROJ_KEY}" /v:"1.0.${TRAVIS_BUILD_NUMBER}" /d:sonar.login="${SONAR_LOGIN}" /o:"${SONAR_ORG}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.pullrequest.branch="${TRAVIS_PULL_REQUEST_BRANCH}" /d:sonar.pullrequest.key="${TRAVIS_PULL_REQUEST}" /d:sonar.pullrequest.base="${TRAVIS_PULL_REQUEST_SLUG}" /d:sonar.cs.vstest.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.trx" /d:sonar.cs.xunit.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.xml" /d:sonar.cs.opencover.reportsPaths="$pathResourcesTests/BuildReports/Coverage/coverage.opencover.xml,$pathWebAPITests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.cs.opencover.it.reportsPaths="$pathIntegrationTests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.links.ci="${LINKS_CI}" /d:sonar.links.issue="${LINKS_ISSUE}" /d:sonar.links.scm="${LINKS_SCM}"
+  dotnet $runnerSonarScanner begin /n:"${SONAR_PROJ_NAME}" /k:"${SONAR_PROJ_KEY}" /v:"$version.${TRAVIS_BUILD_NUMBER}" /d:sonar.login="${SONAR_LOGIN}" /o:"${SONAR_ORG}" /d:sonar.host.url="${SONAR_HOST}" /d:sonar.pullrequest.branch="${TRAVIS_PULL_REQUEST_BRANCH}" /d:sonar.pullrequest.key="${TRAVIS_PULL_REQUEST}" /d:sonar.pullrequest.base="${TRAVIS_PULL_REQUEST_SLUG}" /d:sonar.cs.vstest.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.trx" /d:sonar.cs.xunit.reportsPaths="${TRAVIS_BUILD_DIR}/tests/*.Tests/BuildReports/UnitTests/*.xml" /d:sonar.cs.opencover.reportsPaths="$pathResourcesTests/BuildReports/Coverage/coverage.opencover.xml,$pathWebAPITests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.cs.opencover.it.reportsPaths="$pathIntegrationTests/BuildReports/Coverage/coverage.opencover.xml" /d:sonar.links.ci="${LINKS_CI}" /d:sonar.links.issue="${LINKS_ISSUE}" /d:sonar.links.scm="${LINKS_SCM}"
 fi
 
 # Build
-dotnet build --configuration Release -p:Version=1.0.${TRAVIS_BUILD_NUMBER} --force
+dotnet build -c $netConfiguration -f $netCoreTargetVersion -p:Version=$version.${TRAVIS_BUILD_NUMBER} --force
 
 # Test & Code Coverage
-dotnet test $pathResourcesTests/PoC.SharpDiff.Resources.Tests.csproj --configuration Release --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory $pathResourcesTests/BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=$pathResourcesTests/BuildReports/Coverage/ "/p:CoverletOutputFormat=\"cobertura,opencover\"" /p:Exclude="[xunit.*]*"
-dotnet test $pathWebAPITests/PoC.SharpDiff.WebAPI.Tests.csproj --configuration Release --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory $pathWebAPITests/BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=$pathWebAPITests/BuildReports/Coverage/ "/p:CoverletOutputFormat=\"cobertura,opencover\"" /p:Exclude="[xunit.*]*"
-dotnet test $pathIntegrationTests/PoC.SharpDiff.Tests.csproj --configuration Release --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory $pathIntegrationTests/BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=$pathIntegrationTests/BuildReports/Coverage/ "/p:CoverletOutputFormat=\"cobertura,opencover\"" /p:Exclude="[xunit.*]*"
+dotnet test $pathResourcesTests/PoC.SharpDiff.Resources.Tests.csproj -c $netConfiguration -f $netCoreTargetVersion --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory $pathResourcesTests/BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=$pathResourcesTests/BuildReports/Coverage/ "/p:CoverletOutputFormat=\"cobertura,opencover\"" /p:Exclude="[xunit.*]*"
+dotnet test $pathWebAPITests/PoC.SharpDiff.WebAPI.Tests.csproj -c $netConfiguration -f $netCoreTargetVersion --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory $pathWebAPITests/BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=$pathWebAPITests/BuildReports/Coverage/ "/p:CoverletOutputFormat=\"cobertura,opencover\"" /p:Exclude="[xunit.*]*"
+dotnet test $pathIntegrationTests/PoC.SharpDiff.Tests.csproj -c $netConfiguration -f $netCoreTargetVersion --logger "trx;LogFileName=TestResults.trx" --logger "xunit;LogFileName=TestResults.xml" --results-directory $pathIntegrationTests/BuildReports/UnitTests /p:CollectCoverage=true /p:CoverletOutput=$pathIntegrationTests/BuildReports/Coverage/ "/p:CoverletOutputFormat=\"cobertura,opencover\"" /p:Exclude="[xunit.*]*"
 
 # Send Coverage to Codacy
 java -jar $runnerCodacy report --language CSharp --force-language -r $pathResourcesTests/BuildReports/Coverage/coverage.cobertura.xml --partial
